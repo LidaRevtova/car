2c04fbf8430afe0a7f2c3c499d9fe1632bb60841
LidaRevtova <lida.revtova@yandex.ru>
2020-09-16 16:44:56 +0700

    done

--- 1.py
+++ 1.py
@@ -1,54 +1,78 @@
 class Carbase:
+    """базовый класс"""
 
-    def __init__(self, car_type, brand, photo_le_name, carrying):
+    def __init__(self, car_type=None, brand=None, photo_le_name=None):
+        """метод инициализации"""
         self.car_type = car_type
         self.brand = brand
         self.photo_le_name = photo_le_name
-        self.carrying = float(carrying)
 
     def get_photo_le_ext(self):
+        """метод для получ расширения"""
         num_of_t = self.photo_le_name.find('.')
-        return self.photo_le_name[num_of_t:]
+        if num_of_t != -1:
+            return self.photo_le_name[num_of_t:]
+        return None
 
-class Car:
+class Car(Carbase):
+    """класс машин"""
 
-    def __init__(self, car_type, brand, passenger_seats_count, photo_le_name, carrying):
-        self.passenger_seats_count = int(passenger_seats_count)
-        super.__init__(car_type, brand, photo_le_name, carrying)
+    def __init__(self, car_type, brand, passenger_seats_count, photo_le_name, body_whl=None, carrying=None):
+        """метод инициализации"""
+        super().__init__(car_type, brand, photo_le_name)
+        self.passenger_seats_count = int(passenger_seats_count) or None
+        self.carrying = float(carrying)
 
+class Truck(Carbase):
+    """класс грузовика"""
 
-
-class Truck:
-    def __init__(self, car_type, brand, photo_le_name, carrying, body_whl):
-        super.__init__(car_type, brand, photo_le_name, carrying)
-        self.body_whl = body_whl.split('x')
-        self.body_width = float(self.body_whl[0])
-        self.body_height = float(self.body_whl[1])
-        self.body_length = float(self.body_whl[2])
+    def __init__(self, car_type, brand, passenger_seats_count, photo_le_name, body_whl, carrying):
+        """метод инициализации"""
+        super().__init__(car_type, brand, photo_le_name)
+        self.body_whl = body_whl or None
+        self.carrying = float(carrying)
+        if self.body_whl is not None:
+            self.body_whl = body_whl.split('x')
+            self.body_width = float(self.body_whl[0])
+            self.body_height = float(self.body_whl[1])
+            self.body_length = float(self.body_whl[-1])
+        else:
+            self.body_height, self.body_length, self.body_width = 0.0, 0.0, 0.0
 
     def get_body_volume(self):
+        """метод для получ размера"""
         return self.body_length * self.body_width * self.body_height
 
 
-class Specmachine:
-    def __init__(self, car_type, brand, photo_le_name, carrying, extra):
-        super.__init__(car_type, brand, photo_le_name, carrying)
+class Specmachine(Carbase):
+    """класс спец машин"""
+    def __init__(self, car_type, brand, passenger_seats_count, photo_le_name, body_whl=None, carrying=None, extra=None):
+        """метод инициализации"""
+        super().__init__(car_type, brand, photo_le_name)
         self.extra = extra
-
+        self.carrying = float(carrying)
 
 
 def get_car_list(filename):
+    """"""
     car_list = []
     with open(filename, encoding='utf-8') as f:
         for i in f:
-            line = i[:-1].split(';')
-            car_list.append(line[:-1])
+            line = i.split(';')
+            if line[0] == 'car':
+                car_list.append(Car(*line[:-1]))
+            elif line[0] == 'truck':
+                car_list.append(Truck(*line[:-1]))
+            elif line[0] == 'spec_machine':
+                car_list.append(Specmachine(*line[:-1]))
+            else:
+                pass
+
     return car_list
 
 def main():
-    pass
+    get_car_list('solution.txt')
 
 if __name__ == '__main__':
     main()
 
-print(get_car_list('solution.txt'))
